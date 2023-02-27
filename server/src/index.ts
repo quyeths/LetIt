f727aa0d951370e9dd6293e2bf2e14a283ae4fc1
quyeths <diesmiling2512@gmail.com>
2023-02-27 15:39:05 +0700

    code optimization

--- server/src/index.ts
+++ server/src/index.ts
@@ -31,30 +31,6 @@ const main = async () => {
   app.listen(4000, () =>
     console.log("server started on http://localhost:4000")
   );
-
-  // Cach 1--------------------.
-  //   await RequestContext.createAsync(orm.em, async () => {
-  //     //   const post = orm.em.create(Post, {
-  //     //     title: "my first post 2",
-  //     //     createdAt: "",
-  //     //     updatedAt: "",
-  //     //   });
-  //     //   await orm.em.persistAndFlush(post);
-  //     //   console.log("post------------------", post);
-  //     const posts = await orm.em.find(Post, {});
-  //     console.log(posts);
-  //   });
-  // Cach 1--------------------.
-
-  // Cach 2--------------------.
-  //   const emFork = orm.em.fork();
-  //   const post = emFork.create(Post, { title: "my first post with emFork" });
-  //   await emFork.persistAndFlush(post);
-  // Cach 2--------------------.
-
-  //   Show all post
-  //   const posts = await emFork.find(Post, {});
-  //   console.log(posts);
 };
 
 main().catch((err) => console.error(err));
